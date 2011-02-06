95726e1e729ed1d50a806aabd110b60ba1e2a3e9
andrew.n.sutton@gmail.com <andrew.n.sutton@gmail.com@3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e>
2011-02-06 15:57:14 +0000

    Adding a range adaptor for keys of associative ranges.
    git-svn-id: http://origin.googlecode.com/svn/trunk@8 3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e

---
+++ range/include/origin/range/key_range.hpp
@@ -0,0 +1,90 @@
+// Copyright (c) 2008-2010 Kent State University
+// Copyright (c) 2011 Texas A&M University
+//
+// This file is distributed under the MIT License. See the accompanying file
+// LICENSE.txt or http://www.opensource.org/licenses/mit-license.php for terms
+// and conditions.
+
+#ifndef ORIGIN_RANGE_KEY_RANGE_HPP
+#define ORIGIN_RANGE_KEY_RANGE_HPP
+
+#include <iterator>
+
+#include <origin/range/support.hpp>
+
+namespace origin
+{
+  // NOTE: This module introduces a new concept: An Associative_Range. This
+  // is a range who's value type is a key/value pair.
+  // FIXME: Would it be useful to develop key/value abstractions over pairs?
+  // Maybe...
+
+  /**
+   * The key iterator...
+   *
+   * The reference type of the key iteartor is a reference to the map's key
+   * type (which is a const type).
+   */
+  template<typename Iter>
+  class key_iterator
+    : bidirectional_iterator<
+        key_iterator<Iter>,
+        typename std::iterator_traits<Iter>::value_type::first_type const&
+      >
+  {
+    typedef Iter base_iterator;
+  public:
+    typedef typename std::iterator_traits<Iter>::value_type::first_type const&
+      reference;
+  
+    key_iterator(Iter i = base_iterator{})
+      : iter_{i}
+    { }
+    
+    reference dereference() const
+    { return (*iter_).first; }
+    
+    void increment()
+    { ++iter_; }
+    
+    void decrement()
+    { --iter_; }
+    
+  private:
+    base_iterator iter_;
+  };
+  
+  
+  /**
+   * The key range adapts a range of key/value pairs to one that selects only
+   * the keys of the underlying range.
+   */
+  template<typename Range>
+  class key_range
+  {
+    typedef typename range_traits<Range>::iterator base_iterator;
+  public:
+    typedef key_iterator<base_iterator> iterator;
+  
+    key_range(Range& rng)
+      : range_(rng)
+    { }
+    
+    iterator begin() const
+    { return begin(rng); }
+    
+    iterator end() const
+    { return end(rng); }
+  };
+
+  /**
+   * Return a range of keys over the given associative range.
+   */
+  template<typename Range>
+  key_range<Range> keys(Range& rng)
+  { return rng; }
+  
+  template<typename Range>
+  key_range<Range const> keys(Range const& rng)
+  { return rng; }
+}
---
+++ range/test/key_range.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2008-2010 Kent State University
+// Copyright (c) 2011 Texas A&M University
+//
+// This file is distributed under the MIT License. See the accompanying file
+// LICENSE.txt or http://www.opensource.org/licenses/mit-license.php for terms
+// and conditions.
+
+#include <map>
+#include <iostream>
+
+#include <origin/range/key_range.hpp>
+
+using namespace std;
+using namespace origin;
+
+int main()
+{
+  map<int, char> m = {
+    {0, 'a'}, {1, 'b'}, {1, 'c'}
+  };
+  for(auto x : keys(m)) {
+    cout << x << " ";
+  }
+  cout << "\n";
+}
+
