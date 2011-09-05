f451fe5ef4d058e76cf77b0cdd8bc56a6fd730e9
andrew.n.sutton@gmail.com <andrew.n.sutton@gmail.com@3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e>
2011-09-05 20:57:50 +0000

    Restructuring graph data structures. Blocked out an implementation for static graphs.
    git-svn-id: http://origin.googlecode.com/svn/trunk@282 3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e

--- graph/include/origin/graph/dynamic_graph.hpp
+++ graph/include/origin/graph/dynamic_graph.hpp
@@ -5,10 +5,13 @@
 // LICENSE.txt or http://www.opensource.org/licenses/mit-license.php for terms
 // and conditions.
 
-#ifndef ORIGIN_GRAPH_ADJACENCY_LIST_HPP
-#define ORIGIN_GRAPH_ADJACENCY_LIST_HPP
+#ifndef ORIGIN_GRAPH_DYNAMIC_GRAPH_HPP
+#define ORIGIN_GRAPH_DYNAMIC_GRAPH_HPP
 
-#include <origin/graph/adjacency_list/directed.hpp>
-#include <origin/graph/adjacency_list/undirected.hpp>
+#error Not implemented
+
+// The dynamic graph library is an adjacency list data structure that is
+// fully dynamic in that it supports the unrestricted addition and deletion
+// of vertices and edges.
 
 #endif
