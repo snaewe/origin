d568c6f8c2e5cb585150ae8cc6ff9b78d92c5097
bbartmanboost <bbartmanboost@3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e>
2012-04-08 14:53:08 +0000

    finished the initial testing for for the matrix base
    git-svn-id: http://origin.googlecode.com/svn/trunk@434 3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e

--- math/include/origin/blas/detail/matrix/matrix_alloc_base.hpp
+++ math/include/origin/blas/detail/matrix/matrix_alloc_base.hpp
@@ -13,15 +13,18 @@
 
 namespace origin { namespace blas { namespace detail {
 
-/** The basic matrix base class used for handling 
- *  memory allocations. 
+
+/** @name matrix_alloc_base
+ *  @brief The basic matrix base class used for handling memory allocations. 
  */
 template<typename T, typename Alloc>
-class matrix_alloc_base {
-public:
+class matrix_alloc_base
+{
+protected:
     typedef typename Alloc::template rebind<T>::other rebound_alloc_type;
-    typedef typename std::allocator_traits<rebound_alloc_type>::allocator_type allocator_type;
-    typedef typename std::allocator_traits<rebound_alloc_type>::value_type value_type;
+public:
+    typedef Alloc allocator_type;
+    typedef T value_type;
     typedef typename std::allocator_traits<rebound_alloc_type>::pointer pointer;
     typedef typename std::allocator_traits<rebound_alloc_type>::const_pointer const_pointer;
     typedef value_type& reference;
@@ -29,20 +32,59 @@ public:
     typedef typename std::allocator_traits<rebound_alloc_type>::difference_type difference_type;
     typedef typename std::allocator_traits<rebound_alloc_type>::size_type size_type;
     
-private:
-    
-    struct matrix_alloc_impl_base {
+protected:    
+    /** @name matrix_alloc_impl_base 
+     *  @brief The matrix_alloc_impl_base is responsible for inheirting the allocator 
+     *  and managing the instance variables. This class is NOT responsible for actually
+     *  copying things around.
+     */
+    struct matrix_alloc_impl_base
+        :rebound_alloc_type
+    {
+        
+        /** @name Default Constructor
+         *  @brief Default constucts the allocator and instance variables.
+         */
         matrix_alloc_impl_base()
-            :start(0), finish(0)
+            :rebound_alloc_type(),
+            start(0),
+            finish(0)
         { }
         
-        size_type size() {
-            return finish - start;
+        /** @name Allocator Constructor
+         *  @brief Provides a copy constructor for an allocator provided
+         *  another instance of the matrix_alloc_impl_base type.
+         */
+        matrix_alloc_impl_base(rebound_alloc_type const& other)
+            :rebound_alloc_type(other),
+            start(0),
+            finish(0)
+        { }
+        
+        /** @name Move Constructor
+         *  @brief Moves the the rhs into the current memory allocator.
+         */
+        matrix_alloc_impl_base(rebound_alloc_type&& other)
+            :rebound_alloc_type(std::move(other)),
+            start(0),
+            finish(0)
+        { }
+        
+        /** @name Swap
+         *  @brief The swap function used for exchanging two different
+         *  matrix_alloc_impl_base's instance variables.
+         */
+        void swap(matrix_alloc_impl_base& rhs) {
+            using std::swap;
+            swap(start, rhs.start);
+            swap(finish, rhs.finish);
         }
+        
         pointer start;
         pointer finish;
     };
-protected:
+    
+    matrix_alloc_impl_base base_impl;
     
 };
     
--- math/tests/blas/matrix_alloc_base_test.cpp
+++ math/tests/blas/matrix_alloc_base_test.cpp
@@ -7,10 +7,166 @@
 
 #include <origin/blas/detail/matrix/matrix_alloc_base.hpp>
 #include <cassert>
+#include <memory>
 using namespace origin::blas::detail;
+// matrix_alloc_base
+
+
+
+// Global variables associated with the fake
+// allocator.
+void* fake_allocator__ptr_allocation_requested;
+bool fake_allocator__allocate_called = false;
+bool fake_allocator__allocator_copy_called = false;
+bool fake_allocator__default_constructor_called = false;
+bool fake_allocator__copy_other_allocator_called = false;
+std::size_t fake_allocator__allocation_size = 0;
+void* fake_allocator__ptr_deallocation_address = 0;
+std::size_t fake_allocator__deallocation_size = 0;
+bool fake_allocator__destructor_called = false;
+bool fake_allocator__deallocate_called = false;
+bool fake_allocator__allocator_move_called = false;
+
+void reset_allocator_test_variables() {
+    fake_allocator__ptr_allocation_requested = (void*)0xdeadbeef;
+    fake_allocator__allocator_move_called = false;
+    fake_allocator__allocator_copy_called = false;
+    fake_allocator__default_constructor_called = false;
+    fake_allocator__copy_other_allocator_called = false;
+    fake_allocator__allocation_size = 0;
+    fake_allocator__ptr_deallocation_address = (void*)0;
+    fake_allocator__deallocation_size = 0;
+    fake_allocator__destructor_called = false;
+    fake_allocator__allocate_called = false;
+    fake_allocator__deallocate_called = false;
+}
+
+template <class T>
+class fake_allocator {
+public:
+    typedef std::size_t    size_type;
+    typedef std::ptrdiff_t difference_type;
+    typedef T*        pointer;
+    typedef const T*  const_pointer;
+    typedef T&        reference;
+    typedef const T&  const_reference;
+    typedef T         value_type;
+
+    template <class U>
+    struct rebind {
+        typedef fake_allocator<U> other;
+    };
+
+    fake_allocator() throw() {
+        fake_allocator__default_constructor_called = true;
+    }
+
+    fake_allocator(fake_allocator&&) throw() {
+        fake_allocator__allocator_move_called = true;
+    }
+    
+    fake_allocator(const fake_allocator&) throw() {
+        fake_allocator__allocator_copy_called = true;
+    }
+
+    template <class U>
+    fake_allocator(const fake_allocator<U>&) throw() {
+        fake_allocator__copy_other_allocator_called = true;
+    }
+
+    ~fake_allocator() throw() {
+        fake_allocator__destructor_called = true;
+    }
+    
+    pointer allocate(size_type n, void* = 0) {
+        fake_allocator__allocate_called = true;
+        fake_allocator__allocation_size = n;
+        return reinterpret_cast<pointer>(fake_allocator__ptr_allocation_requested);
+    }
+
+    void deallocate(pointer p, size_type n) {
+        fake_allocator__deallocation_size = n;
+        fake_allocator__deallocate_called = true;
+        fake_allocator__ptr_deallocation_address = reinterpret_cast<void*>(p);
+    }
+    
+    // Functions not used during testing.
+    size_type max_size() const throw();
+    pointer address(reference x) const;
+    const_pointer address(const_reference x) const;
+    void construct(pointer p, const T& val);
+    void destroy(pointer p);
+};
+
+
+
+// Testing Type.
+template<typename T, typename Alloc>
+struct matrix_alloc_base_allocator_test
+    :matrix_alloc_base<T, Alloc>
+{
+    typedef matrix_alloc_base<T, Alloc> base;
+    typedef typename base::matrix_alloc_impl_base base_impl_type;
+    typedef typename base::rebound_alloc_type allocator_test_type;
+    
+    static void run_tests() {
+        reset_allocator_test_variables();
+        impl_test__default_ctor();
+        reset_allocator_test_variables();
+        impl_test__alloc_copy_ctor();
+        reset_allocator_test_variables();
+        impl_test__alloc_move_ctor();
+        impl_test__swap();
+    }
+    
+    // Impl Tests
+    static void impl_test__default_ctor() {
+        base_impl_type temp;
+        assert(fake_allocator__default_constructor_called);
+        assert(temp.start == 0);
+        assert(temp.finish == 0);
+    }
+    
+    static void impl_test__alloc_copy_ctor() {
+        allocator_test_type temp_alloc;
+        base_impl_type temp(temp_alloc);
+        assert(fake_allocator__allocator_copy_called);
+        assert(temp.start == 0);
+        assert(temp.finish == 0);
+    }
+    
+    static void impl_test__alloc_move_ctor() {
+        allocator_test_type temp_alloc;
+        base_impl_type temp(std::move(temp_alloc));
+        assert(fake_allocator__allocator_move_called);
+        assert(temp.start == 0);
+        assert(temp.finish == 0);
+    }
+    
+    static void impl_test__swap() {
+        base_impl_type temp1;
+        temp1.start = (T*)0xdeadbeef;
+        temp1.finish = (T*)0xdeadb0de;
+        base_impl_type temp2;
+        temp2.start = (T*)0xdef1c8e;
+        temp2.finish = (T*)0xc0ffee;
+        temp1.swap(temp2);
+        assert(temp1.start == (T*)0xdef1c8e);
+        assert(temp1.finish == (T*)0xc0ffee);
+        assert(temp2.start == (T*)0xdeadbeef);
+        assert(temp2.finish == (T*)0xdeadb0de);
+    }
+    
+    
+};
 
 int main() {
-    matrix_alloc_base<float, std::allocator<float>> temp;
+    // matrix_alloc_base<float, std::allocator<float>> temp;
+    
+    {
+        typedef matrix_alloc_base_allocator_test<float, fake_allocator<float>> allocator_test_type;
+        allocator_test_type::run_tests();
+    }
     return 0;
     
 }
