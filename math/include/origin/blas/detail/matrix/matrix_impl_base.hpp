e5cbbd85b018ffbf6543e86f488de01408cb6ab0
bbartmanboost <bbartmanboost@3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e>
2012-04-15 18:16:31 +0000

    done working for now
    git-svn-id: http://origin.googlecode.com/svn/trunk@451 3dbf15f1-5c06-4b2d-4fb8-2ed049db4e7e

--- math/include/origin/blas/detail/matrix/matrix_impl_base.hpp
+++ math/include/origin/blas/detail/matrix/matrix_impl_base.hpp
@@ -13,7 +13,32 @@ namespace origin
     struct matrix_impl_base
         :public matrix_alloc_base<T, Alloc>
     {
-        
+        /*
+    matrix_base()
+        : first(nullptr), last(first)
+      { }
+      
+      matrix_base(size_type n)
+        : first(n > 0 ? new value_type[n] : nullptr), last(first + n)
+      { }
+      
+      // CopyConstructible
+      matrix_base(matrix_base const& x)
+        : first(new value_type[x.size()]), last(first + x.size())
+      { 
+        std::copy(x.first, x.last, first); 
+      }
+      
+      // Move Constructible
+      matrix_base(matrix_base&& x)
+        : first(x.first), last(x.last)
+      { 
+        x.first = x.last = nullptr; 
+      }
+      
+      // Destructible
+      ~matrix_base() { delete [] first; }
+         */
     };
     
 } // end origin
